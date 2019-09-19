86ef963e7cfd2240d1cf9c1048723ab4940ce407
dizsart <darogadibia@gmail.com>
2019-09-19 15:52:41 +0100

    updated files

--- admin.js
+++ admin.js
@@ -22,9 +22,47 @@ $(document).ready(function() {
         },
         success: function() {
           $('.adminmessage').html('Admin added');
+          window.location.assign('admin.html');
         },
       });
     });
   })
-  
+  $('.adminBtn').click(function(event) {
+    event.preventDefault();
+    const password = $('#password').val();
+    const adminID = $('#adminID').val();
+    if (!password || !adminID) {
+      $('.adminmessage1').html('Kindly fill in all fields');
+      return; 
+    }
+    //Check if the user is in the database
+    $.ajax({
+      method: 'GET',
+      url: `http://localhost:3000/users?adminID=${adminID}&password=${password}`,
+      data: {
+        adminID: adminID,
+        password: password,
+      },
+      beforeSend: function() {
+        $('.adminmessage1').html('Loading....');
+      },
+      success: function(response) {
+        if (!response.length) {
+          $('.adminmessage1').html('Login sucessful');
+          localStorage.setItem('email', adminID);
+          //redirect to home page if the login is successfull
+          window.location.assign('adminhome.html');
+        } else {
+          $('.adminmessage1').html('Username or password Incorrect');
+        }
+      },
+    });
+  });
+  //Logout Function
+  $('.logoutBtn1').click(function() {
+    //clear the localstorage and redirect to signup page
+    localStorage.clear();
+    $('.checkLogin').html('Kindly login');
+    window.location.assign('admin.html');
+  })
   
--- order.js
+++ order.js
@@ -7,8 +7,9 @@ $(document).ready(function() {
       const foodorder = $('#foodorder').val();
       const phonenumber = $('#phonenumber').val();
       const location = $('#location').val();
+      const quantity = $('#quantity').val();
       //Check if user input is empty
-      if (!fullname || !foodorder || !phonenumber|| !location) {
+      if (!fullname || !foodorder || !phonenumber|| !location || !quantity) {
         $('.ordermessage').html('Kindly fill in all fields');
         return;
       }
@@ -20,15 +21,74 @@ $(document).ready(function() {
           foodorder,
           phonenumber,
           location,
+          quantity
         },
         beforeSend: function() {
           $('.ordermessage').html('Loading....');
+          localStorage.setItem('fullname', fullname);
+          localStorage.setItem('foodorder', foodorder);
+          localStorage.setItem('phonenumber', phonenumber);
+          localStorage.setItem('location', location);
+          localStorage.setItem('location', quantity);
         },
         success: function() {
-          $('.ordermessage').html('Food order Sent');
+          $('.ordermessage').html('Food order Sent.');
+          alert("You will recieve a call from our representative shortly to confirm your order!!!")
+
         },
+        
       });
+      $('.view1').html("Name: "+fullname);
+      $('.view2').html("Food: "+foodorder);
+      $('.view3').html("Phone Number: "+phonenumber);
+      $('.view4').html("Your location: "+location);
+      $('.view5').html("Quantity Ordered: "+quantity);
+      
+      $('.viewBtn').click(function(event) {
+        event.preventDefault();
+        const fullname=$('#fullname').val();
+      $.ajax({
+        method: 'GET',
+        url: `http://localhost:3000/orders?fullname=${fullname}`,
+        data: { 
+         fullname: fullname,
+          foodorder:foodorder,
+          phonenumber:phonenumber,
+          location:location,
+        },
+        success: function(response) {
+          if (response.length) {
+            $('.view1').html(fullname);
+            $('.view2').html(foodorder);
+            $('.view3').html(phonenumber);
+            $('.view4').html(location);
+          } else {
+            $('.view5').html('No orders made!');}
+          }
+          
+        })      
     });
   })
-  
-  
+
+    
+
+        let searchUrls = new URLSearchParams(window.location.search);
+let idn=searchUrls.get('id');
+console.log(idn)
+$('.updateSubmitBtn').click(function(event) {
+    event.preventDefault();
+const foodname = $('#food').val();
+const prices = $('#prices').val();
+$.ajax({
+    method:"PATCH",
+    url:`http://localhost:3000/food/${idn}`,
+    data:{
+        foodname: foodname,
+        prices: prices
+        
+    },
+
+})
+})
+
+})
